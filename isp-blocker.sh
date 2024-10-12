e2bb22b9f88d7d856f08c3b8e8a20072c97cef08
Sina B <135529333+meta-syfu@users.noreply.github.com>
2024-10-13 01:05:32 +0330

    Update isp-blocker.sh

--- isp-blocker.sh
+++ isp-blocker.sh
@@ -1,164 +1,192 @@
 #!/bin/bash
 
-# Colors for output
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-ENDCOLOR="\e[0m"
+C_CHARTREUSE1="\033[38;5;118m"
+C_SPRINGGREEN2="\033[38;5;42m"
+C_INDIANRED1="\033[38;5;204m"
+C_SKYBLUE2="\033[38;5;111m"
+C_THISTLE1="\033[38;5;225m"
+C_LIGHTSALMON1="\033[38;5;216m"
+C_PALETURQUOISE1="\033[38;5;159m"
+C_YELLOW="\033[38;5;11m"
+C_DARKSLATEGRAY3="\033[38;5;116m"
+C_RED1="\033[38;5;196m"
+C_MAGENTA2="\033[38;5;200m"
+C_WHITE="\033[38;5;15m"
+C_DARKORANGE="\033[38;5;208m"
 
-# Array of ISPs and URLs to download IP lists
-declare -A ISPS
-ISPS=(
-    ["asiatech"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/asiatech.ipv4"
-    ["mci"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mci.ipv4"
-    ["mobinnet"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mobinnet.ipv4"
-    ["mtn"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mtn.ipv4"
-    ["parsan"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/parsan.ipv4"
-    ["pishgaman"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/pishgaman.ipv4"
-    ["ritel"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/ritel.ipv4"
-    ["shatel"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/shatel.ipv4"
-    ["tci"]="https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/tci.ipv4"
-)
-
-# Create /etc/iptables directory if it does not exist
-mkdir -p /etc/iptables
-
-# Function to download IP lists
-download_ip_list() {
-    ISP_NAME=$1
-    URL=${ISPS[$ISP_NAME]}
-    DEST="/etc/iptables/${ISP_NAME}.ipv4"
-
-    echo -e "${YELLOW}Downloading IP list for ${ISP_NAME}...${ENDCOLOR}"
-    curl -s "$URL" -o "$DEST"
-
-    if [ $? -eq 0 ] && [ -s "$DEST" ]; then
-        echo -e "${GREEN}${ISP_NAME} IP list downloaded successfully.${ENDCOLOR}"
-    else
-        echo -e "${RED}Failed to download ${ISP_NAME} IP list or the file is empty.${ENDCOLOR}"
-        return 1
-    fi
-}
-
-# Function to configure ports for specific ISP
-configure_ports_for_isp() {
-    PORT=$1
-    ISP_NAME=$2
-    echo -e "${YELLOW}Configuring port ${PORT} for ISP ${ISP_NAME}...${ENDCOLOR}"
-
-    if [ ! -f "/etc/iptables/${ISP_NAME}.ipv4" ]; then
-        echo -e "${RED}IP list for ${ISP_NAME} not found. Downloading...${ENDCOLOR}"
-        download_ip_list "$ISP_NAME"
-        if [ $? -ne 0 ]; then
-            echo -e "${RED}Failed to configure port ${PORT} for ISP ${ISP_NAME} due to missing IP list.${ENDCOLOR}"
-            return
-        fi
-    fi
-
-    # Apply ufw rules
-    while read -r IP; do
-        if [ -n "$IP" ]; then
-            sudo ufw allow from "$IP" to any port "$PORT" proto tcp
-        fi
-    done < "/etc/iptables/${ISP_NAME}.ipv4"
-
-    echo -e "${GREEN}Port ${PORT} configured for ISP ${ISP_NAME}.${ENDCOLOR}"
-}
-
-# Function to configure always open ports for all ISPs
-configure_always_open_ports() {
-    PORTS=$1
-    IFS=',' read -ra PORT_ARRAY <<< "$PORTS"
-
-    for PORT in "${PORT_ARRAY[@]}"; do
-        echo -e "${YELLOW}Opening port ${PORT} for all ISPs...${ENDCOLOR}"
-        sudo ufw allow "$PORT"/tcp
-        echo -e "${GREEN}Port ${PORT} is open for all ISPs.${ENDCOLOR}"
-    done
-}
-
-# Function to clear all ufw rules
-clear_all_config() {
-    echo -e "${YELLOW}Clearing all ufw rules...${ENDCOLOR}"
-    sudo ufw reset
-    echo -e "${GREEN}All ufw rules have been cleared.${ENDCOLOR}"
-}
-
-# Function to list ISPs and let the user select one
-select_isp() {
-    echo "Select an ISP:"
-    select ISP_NAME in "${!ISPS[@]}"; do
-        if [[ -n "${ISPS[$ISP_NAME]}" ]]; then
-            echo -e "${YELLOW}You selected ${ISP_NAME}.${ENDCOLOR}"
-            break
-        else
-            echo -e "${RED}Invalid selection, please try again.${ENDCOLOR}"
-        fi
-    done
-    echo "$ISP_NAME"
-}
-
-# Main menu function
-main_menu() {
+if [[ $EUID -ne 0 ]]; then
     clear
-    echo "==================== ISP Blocker ===================="
-    echo "1) Configure ports for specific ISPs"
-    echo "2) Configure always open ports"
-    echo "3) Clear all ufw configurations"
-    echo "4) Exit"
-    echo "====================================================="
-    echo -n "Please select an option: "
-    read OPTION
+    echo -e "${C_RED1}You should run this script with root!"
+    echo -e "${C_RED1}Use sudo -i to change user to root!"
+    exit 1
+fi
 
-    case $OPTION in
-        1)
-            echo -n "Enter the ports (comma-separated) to configure for specific ISPs: "
-            read PORTS
-            IFS=',' read -ra PORT_ARRAY <<< "$PORTS"
+function Figlet
+{
+    if ! command -v figlet &> /dev/null; then
+        sudo apt update && sudo apt install -y figlet
+    fi
+}
 
-            for PORT in "${PORT_ARRAY[@]}"; do
-                ISP_NAME=$(select_isp)
-                configure_ports_for_isp "$PORT" "$ISP_NAME"
-            done
-            echo "Press enter to return to the menu..."
-            read
-            main_menu
-            ;;
-        2)
-            echo -n "Enter ports to keep open for all connections (comma-separated): "
-            read ALWAYS_OPEN_PORTS
-            configure_always_open_ports "$ALWAYS_OPEN_PORTS"
-            echo "Press enter to return to the menu..."
-            read
-            main_menu
-            ;;
-        3)
-            clear_all_config
-            echo "Press enter to return to the menu..."
-            read
-            main_menu
-            ;;
-        4)
-            exit 0
-            ;;
-        *)
-            echo -e "${RED}Invalid option, please try again.${ENDCOLOR}"
-            main_menu
-            ;;
+function Iptable
+{
+    if ! command -v iptables &> /dev/null; then
+        apt-get update
+        apt-get install -y iptables
+    fi
+
+    if ! dpkg -s iptables-persistent &> /dev/null; then
+        apt-get update
+        apt-get install -y iptables-persistent
+    fi
+}
+
+function main_menu {
+    clear
+    Figlet
+    echo -e "${C_CHARTREUSE1}"
+    figlet "YourScript" 
+
+    echo -e "${C_WHITE}Which ISP do you want block/unblock?"
+    echo ""
+
+    echo -e "${C_DARKSLATEGRAY3}1 - Hamrah Aval"
+    echo -e "${C_YELLOW}2 - Irancell"
+    echo -e "${C_PALETURQUOISE1}3 - Mokhaberat"
+    echo -e "${C_SKYBLUE2}4 - Rightel"
+    echo -e "${C_LIGHTSALMON1}5 - Shatel"
+    echo -e "${C_THISTLE1}6 - AsiaTech"
+    echo -e "${C_INDIANRED1}7 - Pishgaman"
+    echo -e "${C_SPRINGGREEN2}8 - MobinNet"
+    echo -e "${C_DARKORANGE}9 - ParsOnline"
+    echo -e "${C_MAGENTA2}10 - Exit"
+
+    echo -e "${C_WHITE}"
+
+    read -p "Enter your choice : " isp
+    case $isp in
+        1) isp="MCI" blocking_menu ;;
+        2) isp="MTN" blocking_menu ;;
+        3) isp="TCI" blocking_menu ;;
+        4) isp="Rightel" blocking_menu ;;
+        5) isp="Shatel" blocking_menu ;;
+        6) isp="AsiaTech" blocking_menu ;;
+        7) isp="Pishgaman" blocking_menu ;;
+        8) isp="MobinNet" blocking_menu ;;
+        9) isp="ParsOnline" blocking_menu ;;
+        10) echo -e "${C_RED1}"; clear; figlet "BYE"; exit 0 ;;
+        *) 
+        echo -e "${C_RED1}Invalid option!"; main_menu ;;
     esac
 }
 
-# Enable ufw if not already enabled
-enable_firewall() {
-    echo -e "${YELLOW}Enabling ufw...${ENDCOLOR}"
-    sudo ufw enable
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}ufw enabled successfully.${ENDCOLOR}"
+function blocking_menu {
+    clear
+
+    Figlet
+    echo -e "${C_CHARTREUSE1}"
+    figlet "YourScript" 
+    
+    echo ""
+    echo -e "${C_LIGHTSALMON1}1 - Block $isp"
+    echo -e "${C_MAGENTA2}2 - UnBlock All"
+    echo -e "${C_PALETURQUOISE1}3 - Back to Main Menu"
+
+    echo -e "${C_WHITE}"
+
+    read -p "Enter your choice : " choice
+    echo ""
+    
+    case $choice in
+        1) blocker ;;
+        2) unblocker ;;
+        3) main_menu ;;
+        *) 
+        echo -e "${C_RED1}Invalid option press enter!"; blocking_menu ;;
+    esac
+}
+
+function blocker {
+    clear
+    Figlet
+    Iptable
+
+    if ! iptables -L isp-blocker -n >/dev/null 2>&1; then
+        iptables -N isp-blocker
+    fi
+
+    if ! iptables -C INPUT -j isp-blocker &> /dev/null; then
+        iptables -I INPUT -j isp-blocker
+    fi
+
+    clear
+    echo -e "${C_CHARTREUSE1}"
+    figlet "YourScript" 
+
+    echo -e "${C_WHITE}"
+    read -p "Are you sure about blocking $isp ? [Y/N] : " confirm
+    
+    if [[ $confirm == [Yy]* ]]; then
+        clear
+        case $isp in
+        "MCI")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mci.ipv4')
+            ;;
+        "MTN")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mtn.ipv4')
+            ;;
+        "TCI")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/tci.ipv4')
+            ;;
+        "Rightel")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/ritel.ipv4')
+            ;;
+        "Shatel")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/shatel.ipv4')
+            ;;
+        "AsiaTech")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/asiatech.ipv4')
+            ;;
+        "Pishgaman")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/pishgaman.ipv4')
+            ;;
+        "MobinNet")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/mobinnet.ipv4')
+            ;;
+        "ParsOnline")
+            IP_LIST=$(curl -s 'https://raw.githubusercontent.com/meta-syfu/ASN-Blocker/master/parsan.ipv4')
+            ;;
+        esac
+
+        if [ $? -ne 0 ]; then
+            echo -e "${C_WHITE}"
+            echo -e "${C_RED1}Failed to fetch the IP list."
+            read -p "Press enter to return to Menu!" dummy
+        
+            blocking_menu
+        fi
+        
+        # Continue blocking code...
     else
-        echo -e "${RED}Failed to enable ufw.${ENDCOLOR}"
+        echo -e "${C_RED1}Cancelled!"
+        echo ""
+        read -p "Press enter to return to Menu" dummy
+        blocking_menu
     fi
 }
 
-# Run the menu
-enable_firewall
+function unblocker {
+    clear
+    iptables -F isp-blocker
+    iptables-save > /etc/iptables/rules.v4
+    
+    clear
+    echo -e "${C_CHARTREUSE1}"
+    figlet "YourScript" 
+
+    echo -e "${C_CHARTREUSE1}All ISPs UnBlocked successfully!"
+    read -p "Press enter to return to Menu" dummy
+    blocking_menu
+}
+
 main_menu
